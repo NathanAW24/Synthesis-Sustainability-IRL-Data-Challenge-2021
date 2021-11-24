6bc07a13a37d7d9e765c81b717c8af6ef46d4535
Nathan Aldrich Wiryawan <60286483+NathanAW24@users.noreply.github.com>
2021-11-24 16:59:35 +0800

    Add files via upload

---
+++ 2. SQL Database Filtering/IG_EventData_Filtering.sql
@@ -0,0 +1,34 @@
+/******Keywords
+'#sgfood', '#sustainable', '#sgfoodies', 
+'#singaporefood', '#goodfoodpeoplesg', '#vegan', 
+'#meatless', '#plantbasedfoods', '#eatwideawake', 
+'#homecoking', '#cookingmadeeasy', '#plantbaseddiet', 
+'#igsgfoodies', '#goodfoodpeople', '#vegetarian', 
+'#sustainability', 
+'#crustsingapore', '#goodpeople', '#upcycle', '#foodphotography'  ******/
+
+
+SELECT [post_type],[post_caption],[post_hashtags],[post_comments],[post_likes],[segment_name]
+FROM [Sustainability_Challenge_DB].[dbo].[Event_Data_IG]
+WHERE [post_hashtags] LIKE '%#sgfood%'
+OR [post_hashtags] LIKE '%#sustainable%'
+OR [post_hashtags] LIKE '%#sgfoodies%'
+OR [post_hashtags] LIKE '%#singaporefood%'
+OR [post_hashtags] LIKE '%#goodfoodpeoplesg%'
+OR [post_hashtags] LIKE '%#vegan%'
+OR [post_hashtags] LIKE '%#meatless%'
+OR [post_hashtags] LIKE '%#plantbasedfoods%'
+OR [post_hashtags] LIKE '%#eatwideawake%'
+OR [post_hashtags] LIKE '%#homecoking%'
+OR [post_hashtags] LIKE '%#cookingmadeeasy%'
+OR [post_hashtags] LIKE '%#plantbaseddiet%'
+OR [post_hashtags] LIKE '%#igsgfoodies%'
+OR [post_hashtags] LIKE '%#goodfoodpeople%'
+OR [post_hashtags] LIKE '%#vegetarian%'
+OR [post_hashtags] LIKE '%#sustainability%'
+OR [post_hashtags] LIKE '%#crustsingapore%'
+OR [post_hashtags] LIKE '%#goodpeople%'
+OR [post_hashtags] LIKE '%#upcycle%'
+OR [post_hashtags] LIKE '%#foodphotography%'
+--GROUP BY [segment_name]
+ORDER BY [post_likes] DESC
---
+++ 2. SQL Database Filtering/TW_EventData_Filtering.sql
@@ -0,0 +1,16 @@
+/****** KEYWORDS
+['surplusfood', 'Courtauld2025', 'makegoodhappen', 'christmasleftovers', 'plantbased']
+
+******/
+
+SELECT [post_content],[post_is_retweet],[post_is_truncated],[post_like_count],
+[post_retweet_count],[post_media],[post_hashtags],[post_reply_to_post_id],[segment_name]
+FROM [Sustainability_Challenge_DB].[dbo].[Event_Data_TW]
+WHERE [post_hashtags] LIKE '%surplusfood%'
+OR [post_hashtags] LIKE '%Courtauld2025%'
+OR [post_hashtags] LIKE '%christmasleftovers%'
+OR [post_hashtags] LIKE '%plantbased%'
+--ORDER BY [post_like_count] DESC
+ORDER BY cast([post_retweet_count] as int) DESC --post_retweet_count may be more important to determine significance in this one
+
+--IG might be a better tool to promote food surplus due to the small significance of these posts in TW
